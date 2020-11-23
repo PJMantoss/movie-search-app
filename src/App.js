f1b0c70209cb94b12d83bc1d6c975acacf3d1109
pjmantoss <joelptoss@gmail.com>
2020-11-23 14:56:09 +0100

    update App.css

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
   render(){
     return (
       <div className="app">
-        <h1>Hello World!</h1>
+        <h1 className="title">React Movie Search</h1>
       </div>
     );
   }

84e928690d49840faa7eb8812481bc1220aed15f
pjmantoss <joelptoss@gmail.com>
2020-11-23 15:11:04 +0100

    update SearchMovies

--- src/SearchMovie.js
+++ src/SearchMovie.js
@@ -5,7 +5,7 @@ function SearchMovie(){
         <div>
             <form className="form">
                 <label htmlFor="query">
-                    <input type="text" name="query" id="query" placeholder="type movie name"/>
+                    <input className="input" type="text" name="query" id="query" placeholder="type movie name e.g Nutty Professor"/>
                 </label>
                 <input type="submit" value="Search"/>
             </form>

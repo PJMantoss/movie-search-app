b5e0278da84e38bb3deecc37781ee2153ad12400
pjmantoss <joelptoss@gmail.com>
2020-11-24 14:15:44 +0100

    update Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ function Footer(){
             <div id="love">
                 <span>Made With</span>
                 <img src="https://img.icons8.com/dusk/64/000000/like.png" alt="love"/> 
-                
+                <span>by <a href="https://github.com/PJMantoss" target="_blank">Mantoss</a></span>
             </div>
         </footer>
     )

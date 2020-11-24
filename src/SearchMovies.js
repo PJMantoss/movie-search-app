0c1304befd41917688beaf66d16166f44af48d3c
pjmantoss <joelptoss@gmail.com>
2020-11-24 12:11:29 +0100

    update movie state in searchMovies

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,21 +1,22 @@
 import React, { useState } from 'react';
 
 function SearchMovies(){
-
+    //state for input query
     const [query, setQuery] = useState('');
+    //state for movies
     const [movies, setMovies] = useState([]);
 
     const searchMovies = async (e) => {
         e.preventDefault();
 
-        const query = "The Mechanic";
+        // const query = "The Mechanic";
 
         const url = `https://api.themoviedb.org/3/search/movie?api_key=9b9e25dbc1c45abfea8f24e2af2091ff&language=en-US&query=${query}&page=1&include_adult=false`;
 
         try{
             const res = await fetch(url);
             const data = await res.json();
-            console.log(data);
+            setMovies(data.results);
         }catch(err){
             console.error(err);
         }
@@ -25,7 +26,13 @@ function SearchMovies(){
         <div>
             <form className="form" onSubmit={searchMovies}>
                 <label className="label" htmlFor="query"> Movie Name</label>
-                <input className="input" type="text" name="query" placeholder="movie name e.g Nutty Professor"/>
+                <input 
+                    className="input" 
+                    type="text" 
+                    name="query"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)} 
+                    placeholder="movie name e.g Nutty Professor"/>
                 <input className="button type" type="submit" value="Search"/>
             </form>
         </div>

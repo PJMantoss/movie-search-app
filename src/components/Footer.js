2cabf7fe01e9f8f348f4c48723a884057baf28a1
pjmantoss <joelptoss@gmail.com>
2020-11-24 14:14:43 +0100

    update Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,9 @@ import React from 'react';
 function Footer(){
     return (
         <footer>
+            <p><strong>Beer App</strong></p>
+            <p>Copyright 2020</p>
+
             
         </footer>
     )

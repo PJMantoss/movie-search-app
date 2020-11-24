59e9c6027cde5571b8267fb0dff84856994c6cf9
pjmantoss <joelptoss@gmail.com>
2020-11-24 08:31:21 +0100

    update searchMovies

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -7,9 +7,7 @@ function SearchMovies(){
 
         const query = "The Mechanic";
 
-        const url = `https://api.themoviedb.org/3/search/movie?
-                    api_key=6a711b2f9574da2458063937e061f1c3=en-US&query=${query}&page=1&
-                    include_adult=false`;
+        const url = `https://api.themoviedb.org/3/movie/550?api_key=6a711b2f9574da2458063937e061f1c3=en-US&query=${query}&page=1&include_adult=false`;
 
         const res = await fetch(url);
         const data = await res.json();

d9a92c5e40c5657a5f0b9695b4ab3636dc9eb247
pjmantoss <joelptoss@gmail.com>
2020-11-24 13:52:40 +0100

    update MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -6,14 +6,14 @@ function MovieCard(){
             <div className="card" key={movie.id}>
                 <img 
                     className="card--image" 
-                    src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${.info.movie.poster_path}`} 
-                    alt={.info.movie.title + ' poster'} 
+                    src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} 
+                    alt={movie.title + ' poster'} 
                 />
                 <div className="card--content">
-                    <h3 className="card--title">{.info.movie.title}</h3>
-                    <p><small>RELEASE DATE: {.info.movie.release_date}</small></p>
-                    <p><small>RATING: {.info.movie.vote_average}</small></p>
-                    <p className="card--desc">{.info.movie.overview}</p>
+                    <h3 className="card--title">{movie.title}</h3>
+                    <p><small>RELEASE DATE: {movie.release_date}</small></p>
+                    <p><small>RATING: {movie.vote_average}</small></p>
+                    <p className="card--desc">{movie.overview}</p>
                 </div>
             </div>
     )

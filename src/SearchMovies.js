c7c88c110729f43fc66d1165378b5c551b2771f2
pjmantoss <joelptoss@gmail.com>
2020-11-24 13:29:00 +0100

    update searchMovies

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -45,7 +45,7 @@ function SearchMovies(){
                             alt={movie.title + ' poster'} 
                         />
                         <div className="card--content">
-                            <h3 className="card.title">{movie.title}</h3>
+                            <h3 className="card--title">{movie.title}</h3>
                             <p><small>RELEASE DATE: {movie.release_date}</small></p>
                             <p><small>RATING: {movie.vote_average}</small></p>
                             <p className="card--desc">{movie.overview}</p>

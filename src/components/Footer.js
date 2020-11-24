05402d1807571695188e27350a7b690c7aac0826
pjmantoss <joelptoss@gmail.com>
2020-11-24 14:14:56 +0100

    update Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer(){
     return (
         <footer>
-            <p><strong>Beer App</strong></p>
+            <p><strong>Movie Search</strong></p>
             <p>Copyright 2020</p>
 
             

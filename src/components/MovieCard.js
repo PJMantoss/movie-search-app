4d6ed71c21c608555a1c351397314f69032d2960
pjmantoss <joelptoss@gmail.com>
2020-11-24 13:52:25 +0100

    update MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
 
-function MovieCard(props){
+function MovieCard(){
 
     return (
             <div className="card" key={movie.id}>
                 <img 
                     className="card--image" 
-                    src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${props.info.movie.poster_path}`} 
-                    alt={props.info.movie.title + ' poster'} 
+                    src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${.info.movie.poster_path}`} 
+                    alt={.info.movie.title + ' poster'} 
                 />
                 <div className="card--content">
-                    <h3 className="card--title">{props.info.movie.title}</h3>
-                    <p><small>RELEASE DATE: {props.info.movie.release_date}</small></p>
-                    <p><small>RATING: {props.info.movie.vote_average}</small></p>
-                    <p className="card--desc">{props.info.movie.overview}</p>
+                    <h3 className="card--title">{.info.movie.title}</h3>
+                    <p><small>RELEASE DATE: {.info.movie.release_date}</small></p>
+                    <p><small>RATING: {.info.movie.vote_average}</small></p>
+                    <p className="card--desc">{.info.movie.overview}</p>
                 </div>
             </div>
     )
